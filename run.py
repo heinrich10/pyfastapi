684984691839d7d8880449448008da02cc609dc4
Heinrich Chan <guiltyrage10@gmail.com>
2024-02-20 02:27:38 +0800

    refactor settings

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import uvicorn
 
-from pyfastapi.config import Config
+from pyfastapi.config import get_config
 from pyfastapi.main import app
 
+Config = get_config()
 
 if __name__ == "__main__":
     uvicorn.run(app, host=Config.HOST, port=Config.PORT, log_level=Config.LOG_LEVEL)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,10 @@
 from pytest import fixture
 from alembic.config import Config
 from alembic import command
-from dotenv import load_dotenv, find_dotenv
+from pyfastapi.config import get_config
 
 pytest_plugins = ["faker"]
-
-
-def pytest_configure():
-    load_dotenv(find_dotenv(".env.test"))
-
-
-def pytest_unconfigure():
-    pass
+AppConfig = get_config(".env.test")
 
 
 @fixture(autouse=True)
@@ -20,7 +13,6 @@ def init_db():
     this is equivalent to "alembic upgrade head" then run "alembic downgrade base" after 1 test is done
     runs every test to make sure we have a clean set of data
     """
-    from pyfastapi.config import Config as AppConfig
     config = Config()
     config.set_main_option("sqlalchemy.url", AppConfig.DB_HOST)
     config.set_main_option("script_location", "alembic")
